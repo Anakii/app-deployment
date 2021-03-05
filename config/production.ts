7a92baba645063e9aa8f6116549108b1dc1da0b1
Or Anaki <or.anaki@ls-techs.com>
2021-03-05 09:53:10 +0200

    change allow origins

--- config/production.ts
+++ config/production.ts
@@ -4,7 +4,7 @@ import { EnviormentConfig, NodeEnvEnum } from "./utils";
 export const productionConfig: EnviormentConfig = {
     ...defaultEnv,
     nodeEnv: NodeEnvEnum.production,
-    allowedOrigins: `https://my-domain:${defaultEnv.port}`,
+    allowedOrigins: `*`,
     mongoDBName:'deploy-prod',
     mongoPass:'or_1234',
     mongoUser:'or_1234'
